ee3cfa60d280093841aa2660ebee5096fd3b89a1
zairulmazwan <zairulmazwan@gmail.com>
2023-12-01 09:36:16 +0000

    Updates

--- ADSLabWeek10/Experiment.cs
+++ ADSLabWeek10/Experiment.cs
@@ -2,10 +2,11 @@ public class Experiment
 {
     public static void runExp()
     {
+        //You may need to declare a 2D array of size [25,3] to capture Datasets, Experiment Number, Seconds//You may need to declare a 2D array of size [25,3] to capture Datasets, Experiment Number, Seconds
         for (int n=1000; n<10000; n+=2000)
         {
             //Read the dataset of size n
-            //You may need to declare a 2D array of size [25,3] to capture Datasets, Experiment Number, Seconds
+
 
             Console.WriteLine("::Experiment for Dataset {0}::", n);
             for (int exp=1; exp<6; exp++)
